8fb46a0036d0277e4686e708c408cfa0e973eab0
Jeff Eck <jeff.eck.85@gmail.com>
2019-05-15 21:29:36 -0500

    Created user app

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post 
 
 # Register your models here.
+# This adds the model to the admin panel
+admin.site.register(Post)
+
+
+
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,29 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post 
+
 
 # Testing data 
 # Get this from database, csv, etc.
-posts = [
-    {
-        'author': 'Jeff',
-        'title': 'Post 1',
-        'content': 'First post content',
-        'date_posted': 'May 14, 2019'
-    },
-        {
-        'author': 'John',
-        'title': 'Post 2',
-        'content': 'Second post content',
-        'date_posted': 'May 14, 2019'
-    }
-
-]
+# posts = [
+#     {
+#         'author': 'Jeff',
+#         'title': 'Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'May 14, 2019'
+#     },
+#         {
+#         'author': 'John',
+#         'title': 'Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'May 14, 2019'
+#     }
+# ]
 
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts 
+        # 'posts': posts
+        'posts': Post.objects.all() 
     }
     # return HttpResponse('<h1>Blog Home</h1>')
     return render(request, 'blog/home.html', context)
