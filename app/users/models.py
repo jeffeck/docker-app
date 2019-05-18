b30e021c0caa04362529cf1c2d09883d1ea1e76c
Jeff Eck <jeff.eck.85@gmail.com>
2019-05-18 11:07:28 -0500

    Initial commit

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User 
+
 
 # Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
